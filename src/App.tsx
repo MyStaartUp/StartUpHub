a47e94f7eae2590e28609a2eda7b77f124cba810
manuelebeh <emmanuelebehdev@gmail.com>
2024-12-24 10:55:14 +0000

    Ajout de l'indication active sur les menus, fix de la page register-startup qui ne s'affichait pas

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,27 @@ import { StartupPage } from '@/pages/StartupPage';
 import { StartupDetailsPage } from '@/pages/StartupDetailsPage';
 import { AboutPage } from '@/pages/AboutPage';
 import { RegisterStartupPage } from '@/pages/RegisterStartupPage';
+import { AuthProvider } from './features/auth/components/AuthProvider';
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/investors" element={<InvestorPage />} />
-            <Route path="/startups" element={<StartupPage />} />
-            <Route path="/startups/:id" element={<StartupDetailsPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/register-startup" element={<RegisterStartupPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/investors" element={<InvestorPage />} />
+              <Route path="/startups" element={<StartupPage />} />
+              <Route path="/startups/:id" element={<StartupDetailsPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/register-startup" element={<RegisterStartupPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
